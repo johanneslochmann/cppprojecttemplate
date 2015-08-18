dffb1095f3c3302308c74771c046055b0790d3c6
Johannes Lochmann <johannes.lochmann@gmail.com>
2015-08-18 15:00:14 +0200

    added PortEdit

---
+++ src/lib/ui/widgets/portedit.hxx
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <ui/widgets/intedit.hxx>
+
+PROJECT_NAMESPACE_BEGIN
+WIDGETS_NAMESPACE_BEGIN
+
+class PortEdit : public IntEdit
+{
+    Q_OBJECT
+public:
+    using IntEdit::IntEdit;
+};
+
+WIDGETS_NAMESPACE_END
+PROJECT_NAMESPACE_END
