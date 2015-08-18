68d7d221f6d189a75642d6e3176aee0cf1ac451a
Johannes Lochmann <johannes.lochmann@gmail.com>
2015-08-18 14:57:57 +0200

    added IntValidator

--- src/lib/ui/widgets/intedit.cxx
+++ src/lib/ui/widgets/intedit.cxx
@@ -1,11 +1,14 @@
 #include "intedit.hxx"
 
+#include <QtGui/QIntValidator>
+
 PROJECT_NAMESPACE_BEGIN
 WIDGETS_NAMESPACE_BEGIN
 
 IntEdit::IntEdit(int val, QWidget *p)
     : LineEdit(p)
 {
+    setValidator(new QIntValidator(this));
     setValue(val);
 
     connect(this, &QLineEdit::textChanged, [=](const QString& val){ emit valueChanged(val.toInt()); });
