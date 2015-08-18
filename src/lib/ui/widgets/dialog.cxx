fbc519632e6c77e6255ecaf75d16857b9538fce5
Johannes Lochmann <johannes.lochmann@gmail.com>
2015-08-18 07:47:03 +0200

    added class Dialog

--- src/lib/ui/widgets/dialog.cxx
+++ src/lib/ui/widgets/dialog.cxx
@@ -1,7 +1,26 @@
 #include "dialog.hxx"
 
+#include <QtWidgets/QLayout>
+
 PROJECT_NAMESPACE_BEGIN
 WIDGETS_NAMESPACE_BEGIN
+
+QDialogButtonBox *Dialog::createButtons(QDialogButtonBox::StandardButtons b)
+{
+    m_buttons = new QDialogButtonBox(b, Qt::Horizontal, this);
+
+    connect(m_buttons, &QDialogButtonBox::accepted, this, &QDialog::accept);
+    connect(m_buttons, &QDialogButtonBox::rejected, this, &QDialog::reject);
+
+    appendToMainLayout(m_buttons);
+
+    return m_buttons;
+}
+
+void Dialog::appendToMainLayout(QWidget *w)
+{
+    layout()->addWidget(w);
+}
 
 PROJECT_NAMESPACE_END
 WIDGETS_NAMESPACE_END
--- src/lib/ui/widgets/dialog.hxx
+++ src/lib/ui/widgets/dialog.hxx
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <QtWidgets/QDialog>
+#include <QtWidgets/QDialogButtonBox>
 
 #include <ui/widgets/config.hxx>
 
@@ -12,6 +13,27 @@ class Dialog : public QDialog
     Q_OBJECT
 public:
     using QDialog::QDialog;
+
+protected:
+    template<typename T=QWidget>
+    T* contentWidget() const {
+        return qobject_cast<T*>(m_contentWidget);
+    }
+
+    template<typename T>
+    T* createContentWidget() {
+        m_contentWidget = new T(this);
+        appendToMainLayout(m_contentWidget);
+        return m_contentWidget;
+    }
+
+    QDialogButtonBox* createButtons(QDialogButtonBox::StandardButtons b = QDialogButtonBox::Ok | QDialogButtonBox::Cancel);
+
+    virtual void appendToMainLayout(QWidget* w);
+
+private:
+    QWidget* m_contentWidget { nullptr };
+    QDialogButtonBox* m_buttons { nullptr };
 };
 
 PROJECT_NAMESPACE_END
