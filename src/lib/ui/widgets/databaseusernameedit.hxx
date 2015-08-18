5a8f24f0d42208d3646de06edb629308926ff1a5
Johannes Lochmann <johannes.lochmann@gmail.com>
2015-08-18 15:10:46 +0200

    added DatabaseUserNameEdit

---
+++ src/lib/ui/widgets/databaseusernameedit.hxx
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <ui/widgets/lineedit.hxx>
+
+PROJECT_NAMESPACE_BEGIN
+WIDGETS_NAMESPACE_BEGIN
+
+class DatabaseUserNameEdit : public LineEdit
+{
+    Q_OBJECT
+public:
+    using LineEdit::LineEdit;
+};
+
+WIDGETS_NAMESPACE_END
+PROJECT_NAMESPACE_END
