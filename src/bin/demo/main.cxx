2b94d1ee171ac1c978f3bed532f1b343fd5dcfd5
Johannes Lochmann <johannes.lochmann@gmail.com>
2015-08-18 21:35:41 +0200

    added config.hxx for demo application

--- src/bin/demo/main.cxx
+++ src/bin/demo/main.cxx
@@ -1,7 +1,17 @@
 #include <apps/app/app.hxx>
 #include <ui/widgets/mainwindow.hxx>
 
+#include "config.hxx"
+
 int main(int argc, char** argv) {
+    QCoreApplication::setApplicationName(QString::fromStdString(PROJECT_NAMESPACE::applicationName));
+    QCoreApplication::setApplicationVersion(QString::fromStdString(PROJECT_NAMESPACE::applicationVersion));
+    QCoreApplication::setOrganizationName(QString::fromStdString(PROJECT_NAMESPACE::organizationName));
+    QCoreApplication::setOrganizationDomain(QString::fromStdString(PROJECT_NAMESPACE::organizationDomain));
+    QApplication::setApplicationDisplayName(QObject::tr("%1 V. %2")
+                                            .arg(QCoreApplication::applicationName())
+                                            .arg(QCoreApplication::applicationVersion()));
+
     PROJECT_NAMESPACE::APP_NAMESPACE::App a(argc, argv);
     PROJECT_NAMESPACE::WIDGETS_NAMESPACE::MainWindow w;
 
