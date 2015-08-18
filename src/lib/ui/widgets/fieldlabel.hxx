52c582a64fa30827619b81276c9b8670f4bf05e3
Johannes Lochmann <johannes.lochmann@gmail.com>
2015-08-18 14:29:36 +0200

    added FieldLabel

---
+++ src/lib/ui/widgets/fieldlabel.hxx
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <ui/widgets/label.hxx>
+
+PROJECT_NAMESPACE_BEGIN
+WIDGETS_NAMESPACE_BEGIN
+
+class FieldLabel : public Label
+{
+public:
+    using Label::Label;
+};
+
+WIDGETS_NAMESPACE_END
+PROJECT_NAMESPACE_END
