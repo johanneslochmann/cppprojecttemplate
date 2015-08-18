23adbe793b964595a37d68f1d3ec969597c16e64
Johannes Lochmann <johannes.lochmann@gmail.com>
2015-08-18 15:23:03 +0200

    whitespace cleanup

--- src/lib/db/pgconn/connectiondata.cxx
+++ src/lib/db/pgconn/connectiondata.cxx
@@ -17,6 +17,7 @@ const String ConnectionData::connectionString() const
 {
     return "not implemented";
 }
+
 String ConnectionData::password() const
 {
     return m_password;
@@ -26,6 +27,7 @@ void ConnectionData::setPassword(const String &password)
 {
     m_password = password;
 }
+
 String ConnectionData::userName() const
 {
     return m_userName;
@@ -35,6 +37,7 @@ void ConnectionData::setUserName(const String &userName)
 {
     m_userName = userName;
 }
+
 String ConnectionData::databaseName() const
 {
     return m_databaseName;
@@ -44,6 +47,7 @@ void ConnectionData::setDatabaseName(const String &databaseName)
 {
     m_databaseName = databaseName;
 }
+
 int ConnectionData::port() const
 {
     return m_port;
@@ -53,6 +57,7 @@ void ConnectionData::setPort(int port)
 {
     m_port = port;
 }
+
 String ConnectionData::hostName() const
 {
     return m_hostName;
@@ -62,11 +67,6 @@ void ConnectionData::setHostName(const String &hostName)
 {
     m_hostName = hostName;
 }
-
-
-
-
-
 
 PGCONN_NAMESPACE_END
 PROJECT_NAMESPACE_END
