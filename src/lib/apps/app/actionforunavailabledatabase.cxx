b0fac6d2b88406fcd524e50ff09616769793494a
Johannes Lochmann <johannes.lochmann@gmail.com>
2015-08-18 07:25:44 +0200

    added ActionForUnavailableDatabase

---
+++ src/lib/apps/app/actionforunavailabledatabase.cxx
@@ -0,0 +1,32 @@
+#include "actionforunavailabledatabase.hxx"
+
+#include <apps/app/app.hxx>
+
+PROJECT_NAMESPACE_BEGIN
+APP_NAMESPACE_BEGIN
+
+ActionForUnavailableDatabase::ActionForUnavailableDatabase(const QString &text, QWidget *p, const QKeySequence &seq)
+    : DatabaseAction(text, p)
+{
+    if (!seq.isEmpty()) {
+        setShortcut(seq);
+    }
+
+    connect(App::get(), &App::databaseIsAvailable, this, &ActionForUnavailableDatabase::onDatabaseAvailable);
+    connect(App::get(), &App::databaseIsUnavailable, this, &ActionForUnavailableDatabase::onDatabaseUnavailable);
+
+    onDatabaseUnavailable();
+}
+
+void ActionForUnavailableDatabase::onDatabaseAvailable()
+{
+    setEnabled(false);
+}
+
+void ActionForUnavailableDatabase::onDatabaseUnavailable()
+{
+    setEnabled(true);
+}
+
+APP_NAMESPACE_END
+PROJECT_NAMESPACE_END
---
+++ src/lib/apps/app/actionforunavailabledatabase.hxx
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <apps/app/databaseaction.hxx>
+
+PROJECT_NAMESPACE_BEGIN
+APP_NAMESPACE_BEGIN
+
+class ActionForUnavailableDatabase : public DatabaseAction
+{
+public:
+    explicit ActionForUnavailableDatabase(const QString& text, QWidget* p = nullptr, const QKeySequence& seq = {});
+
+public slots:
+    void onDatabaseAvailable();
+    void onDatabaseUnavailable();
+};
+
+PROJECT_NAMESPACE_END
+APP_NAMESPACE_END
